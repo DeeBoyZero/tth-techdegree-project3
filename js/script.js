0c4740e0322d014775b7357e1cd97e52d66c8cff
Mathieu Desilets <mathieu.desilets@gmail.com>
2020-12-15 14:20:23 -0500

    added focus to name field

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const nameField = document.querySelector('#name');
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    // Set focus on the name fiel on page load.
+    nameField.focus();
+});
+
+
+
